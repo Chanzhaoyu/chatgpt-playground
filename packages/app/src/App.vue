2609e32a95431538bb317864c6402fbf2e9d8f97
ChenZhaoYu <790348264@qq.com>
2023-02-23 21:37:24 +0800

    feat: 功能验证

--- packages/app/src/App.vue
+++ packages/app/src/App.vue
@@ -1,9 +1,85 @@
 <template>
   <div>
-    <button>Button</button>
+    <input v-model="value" />
+    <p>{{ message }}</p>
+    <button @click="handleStart">Start</button>
+    <button @click="handleClick">Close</button>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+const loading = ref(false);
 
-<style></style>
+const message = ref('');
+const value = ref('你是谁');
+
+let source: EventSource;
+
+function handleStart() {
+  if (loading.value || !value.value || value.value.trim() === '') return;
+
+  source?.close();
+
+  loading.value = true;
+
+  const BASE_URL = new URL('http://127.0.0.1:3002/events');
+  BASE_URL.searchParams.append('prompt', value.value);
+  const url = BASE_URL.toString();
+
+  source = new EventSource(url);
+
+  source.addEventListener('message', (event) => {
+    message.value = event.data;
+  });
+
+  source.addEventListener('error', () => {
+    window.console.log('error');
+    loading.value = false;
+  });
+
+  source.addEventListener('open', () => {
+    message.value = 'open';
+  });
+
+  source.addEventListener('close', () => {
+    window.console.log('close');
+    loading.value = false;
+  });
+
+  source.addEventListener('ping', () => {
+    window.console.log('ping');
+  });
+
+  source.addEventListener('pong', () => {
+    window.console.log('pong');
+  });
+
+  source.addEventListener('start', () => {
+    window.console.log('start');
+  });
+
+  source.addEventListener('end', () => {
+    loading.value = false;
+    window.console.log('end');
+  });
+
+  source.addEventListener('cancel', () => {
+    loading.value = false;
+    window.console.log('cancel');
+  });
+
+  source.addEventListener('pause', () => {
+    window.console.log('pause');
+    loading.value = false;
+  });
+
+  source.addEventListener('resume', () => {
+    window.console.log('resume');
+  });
+}
+
+function handleClick() {
+  source?.close();
+}
+</script>
--- packages/service/src/index.ts
+++ packages/service/src/index.ts
@@ -1,6 +1,12 @@
 import express from 'express'
-import type { ChatContext } from './chatgpt'
-import { chatConfig, chatReply } from './chatgpt'
+import type { ChatContext, ChatMessage } from './chatgpt'
+import { chatConfig, chatReply, chatReplyProcess } from './chatgpt'
+
+interface ChatRequest {
+  prompt: string
+  conversationId?: string
+  parentMessageId?: string
+}
 
 const app = express()
 const router = express.Router()
@@ -26,8 +32,64 @@ router.post('/chat', async (req, res) => {
   }
 })
 
-router.post('/chat-process', async (req, res) => {
-  //
+app.get('/chat-process', async (req, res) => {
+  // 设置响应头
+  res.setHeader('Content-Type', 'text/event-stream')
+  res.setHeader('Cache-Control', 'no-cache')
+  res.setHeader('Connection', 'keep-alive')
+
+  const { prompt, conversationId, parentMessageId } = req.query as unknown as ChatRequest
+
+  const options: ChatContext = {}
+
+  if (conversationId)
+    options.conversationId = conversationId
+
+  if (parentMessageId)
+    options.parentMessageId = parentMessageId
+
+  global.console.log(prompt)
+  global.console.log(options)
+
+  try {
+    await chatReplyProcess(prompt, { ...options }, (chat: ChatMessage) => {
+      global.console.log(chat)
+      res.write(`data: ${JSON.stringify(chat)}\n\n`)
+    })
+  }
+  catch (e) {
+    res.write(`data: ${JSON.stringify(e)}\n\n`)
+  }
+  finally {
+    res.end()
+  }
+
+  res.on('close', () => {
+    global.console.log('close')
+  })
+})
+
+// 设置路由，该路由将发送 SSE 数据
+app.get('/events', async (req, res) => {
+  // 设置响应头
+  res.setHeader('Content-Type', 'text/event-stream')
+  res.setHeader('Cache-Control', 'no-cache')
+  res.setHeader('Connection', 'keep-alive')
+
+  const { prompt } = req.query as unknown as { prompt: string }
+
+  res.write('retry: 10000\n\n')
+
+  for (let i = 0; i < 4; i++) {
+    res.write(`data: ${prompt}-${i}\n\n`)
+    await new Promise(resolve => setTimeout(resolve, 1000))
+  }
+
+  res.end()
+
+  res.on('close', () => {
+    global.console.log('close')
+  })
 })
 
 router.post('/config', async (req, res) => {
